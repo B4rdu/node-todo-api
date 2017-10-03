17592e5924927069e5ad77a1dbec9de97f98a6bb
Emanuele Rinaldi <ele146cc@gmail.com>
2017-10-03 09:55:42 -0400

    Add GET testes for /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,7 +34,12 @@ app.get("/todos/:id", (req, res) => {
     if (!ObjectID.isValid(id))
         return res.status(404).send();
 
-    Todo.findById(id).then((todo) => res.send(todo))
+    Todo.findById(id).then((todo) => {
+        if (todo)
+            res.send(todo);
+        else
+            res.status(404).send();
+    })
         .catch((e) => res.status(400).send());
 });
 
